50dec8084a3c0e48adfc1a13a8ff861ed31b2e15
Calvinmuemah <kelvinmuemah855@gmail.com>
2025-05-03 18:02:24 +0300

    modified some files

--- models/User.js
+++ models/User.js
@@ -1,53 +1,12 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
-import validator from 'validator';
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, 'Username is required'],
-    trim: true,
-    minlength: [3, 'Username must be at least 3 characters'],
-    maxlength: [30, 'Username must be at most 30 characters']
-  },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, 'Please provide a valid email address']
-  },
-  phone_number: {
-    type: String,
-    required: [true, 'Phone number is required'],
-    validate: {
-      validator: function (v) {
-        return /^\+?\d{10,15}$/.test(v);
-      },
-      message: 'Please provide a valid phone number'
-    }
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: [8, 'Password must be at least 8 characters'],
-    select: false
-  }
-}, {
-  timestamps: true
-});
+  username: { type: String, required: true, unique: true },
+  email:    { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone_number: { type: String, required: true },
+}, { timestamps: true });
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
-});
-
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
-};
-
-// ✅ Fix for re-definition issue
 const User = mongoose.models.User || mongoose.model('User', userSchema);
 
 export default User;
